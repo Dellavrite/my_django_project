e9526acda31b8b5bca9766585a807540b4a36f5b
Dellavrite <delavrite@gmail.com>
2022-10-25 21:05:44 +0300

    fix load script

--- searchEngine/models.py
+++ searchEngine/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 
 class News(models.Model):
-    post_link = models.CharField(max_length=127)
-    post_name = models.CharField(max_length=127)
+    post_link = models.CharField(max_length=255)
+    post_name = models.CharField(max_length=255)
     sender_name = models.CharField(max_length=31)
-    sender_link = models.CharField(max_length=127)
+    sender_link = models.CharField(max_length=255)
     post_date = models.DateField()
     post_id = models.CharField(max_length=63)
--- searchEngine/utils.py
+++ searchEngine/utils.py
@@ -3,14 +3,13 @@ import os
 
 from django.templatetags.static import static
 
+from searchEngine.models import News
 
-def engine(word, file):
-    with open(f"{os.getcwd()}{static(file)}") as f:
-        text = f.read()
-        split_re = re.compile(r"[.|?|!]")
-        sentences = filter(lambda t: t, [t.strip() for t in split_re.split(text)])
-        template = rf"\b{word.lower()}\b"
-        res = [" "]
-        res.extend([sentence for sentence in sentences if re.search(template, sentence.lower())])
-        return res
+
+def engine(word):
+    sentences = News.objects.values_list("post_name", flat=True)
+    template = rf"\b{word.lower()}\b"
+    res = [" "]
+    res.extend([sentence for sentence in sentences if re.search(template, sentence.lower())])
+    return res
 
