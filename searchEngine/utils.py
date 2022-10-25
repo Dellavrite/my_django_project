ee5f8bdab8466b1ff7da6a8e585bd35884aa3e51
Dellavrite <delavrite@gmail.com>
2022-10-26 00:15:46 +0300

    update utils

--- searchEngine/utils.py
+++ searchEngine/utils.py
@@ -1,7 +1,7 @@
 import re
 import os
 import pymorphy2
-from django.contrib.postgres.search import SearchQuery
+from django.contrib.postgres.search import SearchQuery, SearchVector
 from django.templatetags.static import static
 
 from searchEngine.models import News
@@ -11,6 +11,7 @@ def engine(word):
     morph = pymorphy2.MorphAnalyzer()
     res = [" "]
     morph_words = [f"'{x.word}'" for x in morph.parse(word.lower())[0].lexeme]
+    vector = SearchVector("post_name")
     query = SearchQuery(f"({' | '.join(morph_words)})", search_type="raw")
-    res.extend(News.objects.filter(post_name__search=query))
+    res.extend(News.objects.annotate(search=vector).filter(search=query))
     return res
