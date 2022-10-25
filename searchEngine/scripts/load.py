b03cba02d4aefac54ff66aac885870e3b53eb391
Dellavrite <delavrite@gmail.com>
2022-10-25 17:32:09 +0300

    fix load script

--- searchEngine/scripts/load.py
+++ searchEngine/scripts/load.py
@@ -7,11 +7,10 @@ from searchEngine.models import News
 
 def run():
     with open(os.path.join(BASE_DIR, 'static', 'merged.csv')) as f:
-        read_file = csv.reader(f)
+        read_file = csv.reader(f, delimiter="~")
 
         News.objects.all().delete()
 
         for news in read_file:
-            print(news)
             News.objects.create(post_link=news[0], post_name=news[1], sender_name=news[2],
                                 sender_link=[3], post_date=[4], post_id=[5])
