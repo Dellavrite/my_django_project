46955f037d7661f4252d245128a32bcf72b453d3
Dellavrite <delavrite@gmail.com>
2022-10-27 02:47:42 +0300

    fix updater

--- searchEngine/scripts/create_post_id.py
+++ searchEngine/scripts/create_post_id.py
@@ -8,6 +8,5 @@ from searchEngine.models import News
 def run():
     post_ids = News.objects.values_list("post_id", flat=True)
     with open(os.path.join(BASE_DIR, 'static', "post_ids.csv"), "w") as post_ids_file:
-        post_ids_writer = csv.writer(post_ids_file, delimiter="~")
-        for ix in post_ids:
-            post_ids_writer.writerow(ix)
+        post_ids_writer = csv.writer(post_ids_file, delimiter='~')
+        post_ids_writer.writerow(post_ids)
