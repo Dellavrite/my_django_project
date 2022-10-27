ae306b4153e2f3400013eaba0c0d4fa767880d09
Dellavrite <delavrite@gmail.com>
2022-10-27 19:19:45 +0300

    fix updater

--- searchEngine/scripts/load.py
+++ searchEngine/scripts/load.py
@@ -9,8 +9,6 @@ def run():
     with open(os.path.join(BASE_DIR, 'static', 'merged.csv')) as f:
         read_file = csv.reader(f, delimiter="~")
 
-        News.objects.all().delete()
-
         for news in read_file:
             News.objects.create(post_link=news[0], post_name=news[1], sender_name=news[2],
                                 sender_link=news[3], post_date=news[4], post_id=news[5])
